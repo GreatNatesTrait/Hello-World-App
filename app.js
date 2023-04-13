245bcd5f912de35d21781b8771827f892876fffa
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-04-13 17:05:13 -0400

    first Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.port || 8000;
+
+app.get("/",(req,res)=>{
+    res.json({
+        meta: {
+            status:true,
+            message:"aite",
+            code:200
+        }
+    });
+});
+
+app.listen(port,()=> console.log(`Server running successfully on port ${port}`));
+
+module.exports = app;
