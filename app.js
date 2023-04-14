75df8e46f7bc55902796c30eae5a94fd1f2f5f41
GreatNatesTrait <mclawhornnathan@gmail.com>
2023-04-14 00:37:54 -0400

    idk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.get("/",(req,res)=>{
     res.json({
